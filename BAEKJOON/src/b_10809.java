eb38cefc729aae0b8de8057ac54281fe4dfff019
504 <504@DESKTOP-VU5V3S6>
2022-03-04 12:03:45 +0900

    10809번 완료

--- BAEKJOON/src/b_10809.java
+++ BAEKJOON/src/b_10809.java
@@ -19,11 +19,17 @@ public class b_10809 {
 		
 		//s 변수에 저장되었있는 문자열에 위치를 arr 배열에다 집어넣기
 		for(int i = 0; i < s.length(); i++) {
+			//문자열을 하나씩 꺼내기 위해 문자열변수.charAt() 사용
+			if(arr[s.charAt(i) - 97] > -1) {
+				//arr[s.charAt(i) - 97] -= 1; 
+			}else {arr[s.charAt(i) - 97] = i;}
 			
 		}
 		//System.out.println(s.charAt(0));
 		//System.out.println('b' - 96);
-		System.out.println(Arrays.toString(arr));
+		for(int i = 0; i < arr.length; i++) {
+			System.out.printf("%d ", arr[i]);
+		}
 	}
 
 }

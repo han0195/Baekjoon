9e0d7af89b78f6754aeb2c5d1ea04cc66ece7de8
504 <504@DESKTOP-MQIVQL4>
2022-03-21 21:52:27 +0900

    d

---
+++ BAEKJOON/src/B_1157.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class B_1157 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer to = new StringTokenizer(br.readLine(), " ");
+		ArrayList<String> arr = new ArrayList<>(); 
+		
+		while (to.hasMoreTokens()) {
+			arr.add(to.nextToken());
+		}
+
+			
+		System.out.println(arr.size());
+		
+		
+	
+		
+		
+	}
+}
